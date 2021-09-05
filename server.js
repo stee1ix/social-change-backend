54eff7b5e20e259573fa2012844db57f9586ac01
stee1ix <rituraj.ranjan.42@gmail.com>
2021-09-05 22:37:53 +0530

    enhanced name isAlpha validator, added created_at entry in response

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const knex = require('knex')
-const validator = require('validator')
-const Joi = require('joi')
+const express = require('express');
+const knex = require('knex');
+const validator = require('validator');
+const Joi = require('joi');
 
 const database = knex({
 	client: 'mysql',
@@ -11,51 +11,61 @@ const database = knex({
 		password: '',
 		database: 'socialchange',
 	},
-})
+});
 
 // check database connectivity
 database
 	.raw("SELECT username FROM users WHERE username = 'stee1ix'")
 	.then(() => console.log('MySQL connected '))
 	.catch(e => {
-		console.log('MySQL not connected')
-		console.error(e)
-	})
+		console.log('MySQL not connected');
+		console.error(e);
+	});
 
-const app = express()
+const app = express();
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 
 // root route
 app.get('/', (req, resp) => {
-	resp.send('Welcome to root')
-})
+	resp.send('Welcome to root');
+});
 
 //user register route
 app.post('/register', (req, resp) => {
-	const { username, name, email, password, birth_date } = req.body
+	const { username, name, email, hash, birth_date } = req.body;
 
-	if (!username || !name || !email || !password || !birth_date) {
-		resp.status(400).json('incorrect form submission')
+	if (!username || !name || !email || !hash || !birth_date) {
+		resp.status(400).json('incorrect form submission');
 	}
 
 	if (!validator.isEmail(email))
-		resp.status(400).json('incorrect email format entered')
+		resp.status(400).json('incorrect email format entered');
 	if (!validator.isAlphanumeric(username))
 		resp.status(400).json(
 			'incorrect username, cannot contain special characters'
-		)
-	if (!validator.isAlpha(name)) resp.status(400).json('incorrect name format')
+		);
+	if (!validator.isAlpha(name, 'en-US', { ignore: ' ' }))
+		resp.status(400).json('incorrect name format');
+	if (!validator.isDate(birth_date))
+		resp.status(400).json('incorrect date format');
 
 	database
-		.insert({ username, name, email, password, birth_date })
+		.insert({
+			username,
+			name,
+			email,
+			hash,
+			birth_date,
+			created_at: new Date(),
+		})
 		.into('users')
 		.then(resolve => resp.send(`Registered ${username} in DB`))
-		.catch(e => resp.status(400).json(e))
-})
+		.catch(e => resp.status(400).json(e));
+});
 
 //server listen
 app.listen(process.env.PORT || 5000, () => {
-	console.log('server is running')
-})
+	console.log('server is running');
+});
