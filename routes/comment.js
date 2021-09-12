25a80b6ace5e55037c9b67efe81c89723fed7b83
stee1ix <rituraj.ranjan.42@gmail.com>
2021-09-13 00:05:09 +0530

    update and delete comment done

--- routes/comment.js
+++ routes/comment.js
@@ -26,16 +26,37 @@ router.post('/create', verify, (req, resp) => {
 		.catch(e => resp.status(400).json(e));
 });
 
-router.route('/modify').delete(verify, (req, resp) => {
-	const { comment_id } = req.body;
+router
+	.route('/modify')
+	.delete(verify, (req, resp) => {
+		const { comment_id } = req.body;
 
-	database('comments')
-		.where({ comment_id })
-		.del()
-		.then(deleteCount => {
-			resp.status(200).send(`comment deleted`);
-		})
-		.catch(error => resp.status(400).json(e));
-});
+		database('comments')
+			.where({ comment_id })
+			.del()
+			.then(deleteCount => {
+				if (deleteCount > 0) {
+					resp.send(`comment deleted`);
+				} else {
+					resp.sendStatus(400);
+				}
+			})
+			.catch(e => resp.status(400).json(e));
+	})
+	.patch(verify, (req, resp) => {
+		const { message, comment_id } = req.body;
+
+		database('comments')
+			.update({ message, created_at: new Date() })
+			.where({ comment_id })
+			.then(updateCount => {
+				if (updateCount > 0) {
+					resp.send(`comment updated`);
+				} else {
+					resp.sendStatus(400);
+				}
+			})
+			.catch(e => resp.status(400).json(e));
+	});
 
 module.exports = router;
